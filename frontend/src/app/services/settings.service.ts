5ebf87900ae8bf00b4d595daa51db2bdcee03506
matBentes <mateus1337bentes@gmail.com>
2026-03-06 01:42:56 -0400

    test: trigger ci autofix smoke test

--- frontend/src/app/services/settings.service.ts
+++ frontend/src/app/services/settings.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 export interface AppSettings {
